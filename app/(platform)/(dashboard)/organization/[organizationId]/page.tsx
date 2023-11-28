8bf420d5f2fb2b490ee669c6f37df471a3d31731
setouchi-h <setouchi.kazuki@gmail.com>
2023-11-28 21:52:21 +0900

    add: db

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,5 +1,31 @@
+import { db } from "@/lib/db";
+
 const page = () => {
-  return <div>Org Page</div>;
+  async function create(formData: FormData) {
+    "use server";
+
+    const title = formData.get("title") as string;
+
+    await db.board.create({
+      data: {
+        title,
+      },
+    });
+  }
+
+  return (
+    <div>
+      <form action={create}>
+        <input
+          id="title"
+          name="title"
+          required
+          placeholder="Enter a board title"
+          className="border-black border p-1"
+        />
+      </form>
+    </div>
+  );
 };
 
 export default page;
