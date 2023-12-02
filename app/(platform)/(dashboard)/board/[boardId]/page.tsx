bb424ce99faea91965b212d8848d75171d49e0eb
setouchi-h <setouchi.kazuki@gmail.com>
2023-12-02 18:26:25 +0900

    add: create list

--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,5 +1,45 @@
-const page = () => {
-  return <div>page</div>;
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { ListContainer } from "./_components/list-container";
+
+interface Props {
+  params: {
+    boardId: string;
+  };
+}
+
+const page = async ({ params }: Props) => {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    redirect("/select-org");
+  }
+
+  const lists = await db.list.findMany({
+    where: {
+      boardId: params.boardId,
+      board: {
+        orgId,
+      },
+    },
+    include: {
+      cards: {
+        orderBy: {
+          order: "asc",
+        },
+      },
+    },
+    orderBy: {
+      order: "asc",
+    },
+  });
+
+  return (
+    <div className="p-4 h-hull overflow-x-auto">
+      <ListContainer boardId={params.boardId} data={lists} />
+    </div>
+  );
 };
 
 export default page;
